f7639c252c535d9e64d142867792f648fa01f4b6
cinta <cintatafur@gmail.com>
2023-05-09 18:54:46 +0200

    💚 Fix(cards):visualizando e insertando mis proyectos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyectos.component.scss'],
 })
 export class ProyectosComponent {
-  proyecto = {
-    id: 0,
-    urlPg: 'https://rickmortyangular.web.app/',
-    urlImage: '/assets/images/proyecto/R&M.jpg',
-    urlGH: 'https://github.com/CinTutuDev/RickMorty',
-    ico: '/assets/icos/rym.png',
-    titulo: 'Rick & Morty',
-    descripcion:
-      'Explorador de Personajes de Rick y Morty: Proyecto realizado con Angular y Bootstrap con Integración de la API de Rick y Morty',
-  };
+  
 }
